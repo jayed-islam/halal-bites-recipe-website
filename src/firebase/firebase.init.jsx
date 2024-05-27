d36d5f3e8b864f196888e6981d5a7ab86bb6a54c
jayed-phero <jayed.programminghero@gmail.com>
2024-05-27 11:21:03 +0600

    update firebase setting

--- src/firebase/firebase.init.jsx
+++ src/firebase/firebase.init.jsx
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+/* eslint-disable no-undef */
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyApRVDwtx2iuBXTMa7dSbM5BvnfR2NrALQ",
-  authDomain: "halalbites-recipe.firebaseapp.com",
-  projectId: "halalbites-recipe",
-  storageBucket: "halalbites-recipe.appspot.com",
-  messagingSenderId: "387211588944",
-  appId: "1:387211588944:web:9ae3cb593e63a7ba5d6855",
-  measurementId: "G-5HGV278108",
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 export default app;
